2f3f9129dbb5f4c9769ebd63938d02e713d8fb3d
ksamuel <kevin.samuel@yandex.com>
2017-01-10 16:15:32 +0100

    Non responsive listing ok

--- autcircu/conf.py
+++ autcircu/conf.py
@@ -1,7 +1,25 @@
 
+import locale
+import warnings
+
+from textwrap import dedent
+
 from pathlib import Path
 
 from flask import Flask
+
+LOCALE = ('fr_FR', 'UTF-8')
+
+try:
+    locale.setlocale(locale.LC_ALL, LOCALE)
+except locale.Error:
+    warnings.warn(dedent(f"""
+        Unable to set the locale to "{LOCALE}". The default value
+        ("{locale.getdefaultlocale()}") will be used. It means some of
+        the text may not be translated properly, particularly dates.
+        Either install the proper locale on your OS or change
+        the locale settings in conf.py.
+    """))
 
 PROJECT_DIR = Path(__file__).parent.parent.absolute()
 VAR_DIR = PROJECT_DIR / 'var'
--- autcircu/routes.py
+++ autcircu/routes.py
@@ -1,15 +1,55 @@
+import os
 
-from flask import Flask, render_template, send_from_directory
+from datetime import datetime
+
+from flask import render_template, send_from_directory, request
 
 from .conf import app
+
+MONTHS = [(0, 'tout mois')]
+MONTHS.extend((i, datetime(2008, i, 1).strftime('%B')) for i in range(1, 13))
+AUTH_STATUS = {'both': "émises ou valides",
+               'emitted': 'émises',
+               'active': 'valides'}
+
+
+def to_int(obj, default):
+    try:
+        return int(obj)
+    except (TypeError, ValueError):
+        return default
+
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
+
 @app.route("/autorisations")
 def listing():
-    return render_template('listing.html')
+
+    now = datetime.utcnow()
+    selected_year = to_int(request.args.get('year'), now.year)
+    selected_month = to_int(request.args.get('month'), now.month)
+
+    # TODO: by default "emitted" for admin, and "active" for agents
+    default_status = 'emitted'
+    selected_auth_status = request.args.get('auth_status',
+                                            default_status).lower()
+    if selected_auth_status not in AUTH_STATUS:
+        selected_auth_status = default_status
+
+    return render_template('listing.html',
+                           selected_year=selected_year,
+                           selected_month=selected_month,
+                           end_year=now.year,
+                           start_year=2000,  # TODO: get year from db
+                           type=type,
+                           months=MONTHS,
+                           selected_auth_status=selected_auth_status,
+                           auth_status=AUTH_STATUS,
+                           )
+
 
 @app.route('/favicon.ico')
 def favicon():
