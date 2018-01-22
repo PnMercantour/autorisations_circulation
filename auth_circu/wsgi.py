f880b2f27558fe4f870667755f92e4a421352313
ksamuel <kevin.samuel@yandex.com>
2018-01-22 21:41:53 +0100

    Init db with app

--- auth_circu/wsgi.py
+++ auth_circu/wsgi.py
@@ -5,5 +5,7 @@
 from auth_circu.routes import app  # noqa
 from auth_circu.db.utils import init_db  # noqa
 
+app.config['INIT_APP_WITH_DB'] = True
+
 init_db(app)
 
--- runserver.py
+++ runserver.py
@@ -18,10 +18,11 @@ os.environ.setdefault('AUTH_CIRCU_CONFIG_FILE', args.config_file or "")
 from auth_circu.routes import app  # noqa
 from auth_circu.db.utils import init_db  # noqa
 
-init_db(app)
-
 # help for debugging. Do not use that in producting.
 app.config['DEBUG'] = True
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['INIT_APP_WITH_DB'] = True
+
+init_db(app)
 
 app.run(host=args.host, port=args.port, threaded=True)
